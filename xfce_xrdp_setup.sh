65b358e8ee66d120a07d40b0ca458c3c90b5e43c
Simon Gao <simon.shubin@gmail.com>
2025-01-23 17:19:47 +0100

    removed startxfce from script

--- xfce_xrdp_setup.sh
+++ xfce_xrdp_setup.sh
@@ -2,5 +2,3 @@ sudo apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils -y
 sudo apt install xrdp -y
 sudo systemctl status xrdp
 sudo adduser xrdp ssl-cert
-
-echo "startxfce4" | tee .xsession
