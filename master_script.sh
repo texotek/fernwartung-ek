23a2db3360fd8a5bb0ef90187266cf25107b1278
texotek <fdahmen@proton.me>
2025-01-23 16:37:03 +0100

    add xfce_xrdpc and ufw setup scripts

---
+++ master_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+source ./wireguard-server.sh
+source ./ufw-setup.sh
+source ./xfce_xrdp_setup.sh
---
+++ xfce_xrdp_setup.sh
@@ -0,0 +1,6 @@
+sudo apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils -y
+sudo apt install xrdp -y
+sudo systemctl status xrdp
+sudo adduser xrdp ssl-cert
+
+echo "startxfce4" | tee .xsession
